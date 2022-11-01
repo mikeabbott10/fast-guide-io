6b1520d554674da92f1526a309bb08af4b7803b0
mikeabbott10 <amorelli.lorenzo@gmail.com>
2022-11-01 20:37:12 +0100

    minor fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import Ctx from "../contexts/Ctx";
 
 const Header = () => {
-    const {darkMode, setDarkMode} = useContext(Ctx);
+    const {setDarkMode} = useContext(Ctx);
     const changeDarkMode = () => {
         setDarkMode(prev => {
             return !prev;
@@ -11,16 +11,9 @@ const Header = () => {
     return (
         <div className="header">
             <span style={{margin:"1rem", color:"white", textTransform:"uppercase"}}><strong>R</strong>eact <strong>F</strong>ast <strong>G</strong>uide</span>
-            {!darkMode && 
-                <svg onClick={changeDarkMode} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> 
-                    <path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z" fill="white"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z" fill="white"></path> 
-                </svg>
-            }
-            {darkMode && 
-                <svg onClick={changeDarkMode} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" > 
-                    <path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z" fill="currentColor" /> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z" fill="currentColor" /> 
-                </svg>
-            }
+            <svg onClick={changeDarkMode} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+                <path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z" fill="white"></path> <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z" fill="white"></path> 
+            </svg>
         </div>
     )
 }
--- src/lessons/UsefulLinks.tsx
+++ src/lessons/UsefulLinks.tsx
@@ -5,8 +5,8 @@ const UsefulLinks = () => {
     const {darkMode} = useContext(Ctx)
 
     const externalLinkSvg = darkMode ?
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path d="M18,10.82a1,1,0,0,0-1,1V19a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V8A1,1,0,0,1,5,7h7.18a1,1,0,0,0,0-2H5A3,3,0,0,0,2,8V19a3,3,0,0,0,3,3H16a3,3,0,0,0,3-3V11.82A1,1,0,0,0,18,10.82Zm3.92-8.2a1,1,0,0,0-.54-.54A1,1,0,0,0,21,2H15a1,1,0,0,0,0,2h3.59L8.29,14.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L20,5.41V9a1,1,0,0,0,2,0V3A1,1,0,0,0,21.92,2.62Z"/>
+        <svg style={{color:"white"}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M18,10.82a1,1,0,0,0-1,1V19a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V8A1,1,0,0,1,5,7h7.18a1,1,0,0,0,0-2H5A3,3,0,0,0,2,8V19a3,3,0,0,0,3,3H16a3,3,0,0,0,3-3V11.82A1,1,0,0,0,18,10.82Zm3.92-8.2a1,1,0,0,0-.54-.54A1,1,0,0,0,21,2H15a1,1,0,0,0,0,2h3.59L8.29,14.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L20,5.41V9a1,1,0,0,0,2,0V3A1,1,0,0,0,21.92,2.62Z" fill="white"></path>
         </svg> :
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <path d="M18,10.82a1,1,0,0,0-1,1V19a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V8A1,1,0,0,1,5,7h7.18a1,1,0,0,0,0-2H5A3,3,0,0,0,2,8V19a3,3,0,0,0,3,3H16a3,3,0,0,0,3-3V11.82A1,1,0,0,0,18,10.82Zm3.92-8.2a1,1,0,0,0-.54-.54A1,1,0,0,0,21,2H15a1,1,0,0,0,0,2h3.59L8.29,14.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L20,5.41V9a1,1,0,0,0,2,0V3A1,1,0,0,0,21.92,2.62Z"/>
@@ -15,23 +15,23 @@ const UsefulLinks = () => {
     return (
         <>
             <h3>Useful links</h3>
-            <a style={{display: "flex", textDecoration: "none"}} target="_blank"
+            <a style={{display: "flex", textDecoration: "none"}} target="_blank" rel="noreferrer"
                 href="https://github.com/mikeabbott10/react-fast-guide">
                 React Fast Guide on&nbsp;<strong> Github </strong> &nbsp; {externalLinkSvg}
             </a>
-            <a style={{display: "flex", textDecoration: "none"}} target="_blank"
+            <a style={{display: "flex", textDecoration: "none"}} target="_blank" rel="noreferrer"
                 href="https://www.npmjs.com/package/react-fast-guide">
                 React Fast Guide on&nbsp;<strong> npm </strong> &nbsp; {externalLinkSvg}
             </a>
-            <a style={{display: "flex", textDecoration: "none"}} target="_blank"
+            <a style={{display: "flex", textDecoration: "none"}} target="_blank" rel="noreferrer"
                 href="https://yarn.pm/react-fast-guide">
                 React Fast Guide on&nbsp;<strong> yarn </strong> &nbsp; {externalLinkSvg}
             </a>
-            <a style={{display: "flex", textDecoration: "none"}} target="_blank"
+            <a style={{display: "flex", textDecoration: "none"}} target="_blank" rel="noreferrer"
                 href="https://github.com/mikeabbott10/fast-guide-io/tree/main">
                 This page on&nbsp;<strong> Github </strong> &nbsp; {externalLinkSvg}
             </a>
-            <a style={{display: "flex", textDecoration: "none"}} target="_blank"
+            <a style={{display: "flex", textDecoration: "none"}} target="_blank" rel="noreferrer"
                 href="https://amorelli-lorenzo.it">
                 React Fast Guide author&nbsp; {externalLinkSvg}
             </a>
